5b19803fec6f5077da55db7d461d17fb12970045
azeezahmed <azizahmed648@gmail.com>
2025-02-19 12:27:46 +0530

    [string-calc][test]: empty input and zero output

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,11 +1,18 @@
 describe "String Calculator" do
 
-  context "string_calculator class" do 
+  context "string_calculator class" do
+
+    before(:each) do
+      @calc = String_Calculator.new
+    end
      
-     it "implements a method add()" do 
-        calc = String_Calculator.new
-        expect(calc).to respond_to(:add)
-     end
+      it "implements a method add()" do
+        expect(@calc).to respond_to(:add)
+      end
+
+      it "accepts an string input and return numeric output" do
+        expect(@calc.add('')).to eq(0)
+      end
      
   end 
 end
