75a69ed09876ea6f18151e5a28a43fdeb279195e
azeezahmed <azizahmed648@gmail.com>
2025-02-19 12:34:40 +0530

    [string-calc][test]: return the same numeric if single digit

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,18 +1,22 @@
 describe "String Calculator" do
-
+  before(:each) do
+    @calc = String_Calculator.new
+  end
   context "string_calculator class" do
 
-    before(:each) do
-      @calc = String_Calculator.new
-    end
-     
-      it "implements a method add()" do
-        expect(@calc).to respond_to(:add)
-      end
 
-      it "accepts an string input and return numeric output" do
-        expect(@calc.add('')).to eq(0)
-      end
-     
+    it "implements a method add()" do
+      expect(@calc).to respond_to(:add)
+    end
+
+    it "accepts an string input and return numeric output" do
+      expect(@calc.add('')).to eq(0)
+    end 
   end 
+
+  context "add()" do 
+    it "returns the numeric equivalent for a single number" do 
+      expect(@calc.add('5')).to eq(5)
+    end
+  end
 end
--- src/main.rb
+++ src/main.rb
@@ -1,5 +1,5 @@
 class String_Calculator
   def add(input_str)
-    return 0
+    input_str.size > 0 ? 1 : 0
   end 
 end
