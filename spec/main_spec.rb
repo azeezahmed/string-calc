f79f62efcc821c2a6fea2de7f7064e08bf036916
azeezahmed <azizahmed648@gmail.com>
2025-02-19 13:26:25 +0530

    [string-calc][test]: support for dynamic delimiters

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -35,5 +35,10 @@ describe "String Calculator" do
       expect(@calc.add('1\n2, 3')).to eq(6)
       expect(@calc.add('1\n2, 3, 4\n7\n5')).to eq(22)
     end
+
+    # after // and before a new line
+    it "supports different delimiters represented at the beginning of string" do
+      expect(@calc.add('//;\n1;2')).to eq(3)
+    end
   end
 end
